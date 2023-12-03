b1823b6ea57c6e637b5d636d92bd554383069afd
CrossDarkrix <crosseddarkrix@gmail.com>
2023-12-03 09:28:52 +0900

    パスワードをNoneに設定できない問題の修正など

--- gigafile.py
+++ gigafile.py
@@ -1,40 +1,23 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time,sys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.alert import Alert
+import argparse
 import os
-import pyperclip,subprocess
-
-# コマンドライン引数を取得します（最初の引数はスクリプト自体なので無視します）。
-args = sys.argv[1:]
-
-# ファイルパスを格納するためのリストを初期化します。
-file_paths = ""
-
-# 各引数について確認します。
-for arg in args:
-    if os.path.isdir(arg):
-        print(f"{arg} is a directory. Exiting.")
-    elif os.path.isfile(arg):
-        file_paths += arg + "\n"
-    else:
-        print(f"{arg} does not exist. Exiting.")
-
-# 結果のリストを表示します。
+import pyperclip
+import time
+from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
 
 options = Options()
 options.add_argument("--headless")
 def upload_file_and_get_link(file_path:str, password=None):
         try:
-            service = webdriver.EdgeService(service_args=['--log-level=OFF'],log_output=None)
-            driver = webdriver.Edge(options=options,service=service)
+            service = webdriver.EdgeService(service_args=['--log-level=OFF'], log_output=None)
+            driver = webdriver.Edge(options=options, service=service)
             print("ページを読み込んでいます")
             driver.get("https://gigafile.nu/")
             print(file_path)
             upload_panel = driver.find_element(By.ID, 'upload_panel_button')
-            file_input = upload_panel.find_element(By.XPATH,".//input[@type='file']")
+            file_input = upload_panel.find_element(By.XPATH, ".//input[@type='file']")
             file_input.send_keys(file_path.rstrip('\n'))
             time.sleep(1)
             print("アップロードしています")
@@ -49,23 +32,47 @@ def upload_file_and_get_link(file_path:str, password=None):
                 if all_complete:
                     break
             print("zipにしています")
-            password_field = driver.find_element(By.ID,"zip_dlkey")
+            password_field = driver.find_element(By.ID, "zip_dlkey")
             password_field.send_keys(password)
-            zip_file_field = driver.find_element(By.ID,"zip_file_name")
+            zip_file_field = driver.find_element(By.ID, "zip_file_name")
             zip_file_field.send_keys("FILE")
-            set_button = driver.find_element(By.ID,"matomete_btn")
+            set_button = driver.find_element(By.ID, "matomete_btn")
             driver.execute_script("arguments[0].click();", set_button)
             time.sleep(1)
             alert = Alert(driver)
             alert_text = alert.text
             print(alert_text)
             alert.accept()
-            link_element = driver.find_element(By.XPATH,"//a[@id='matomete_link_btn']")
+            link_element = driver.find_element(By.XPATH, "//a[@id='matomete_link_btn']")
             link = link_element.get_attribute("href")
             print("成功")
             print("クリップボードにコピーされました")
-            return link
-        except Exception as e:
+            return '{}'.format(link)
+        except:
             print("失敗")
             return "共有リンクの作成に失敗しました"
-pyperclip.copy(str(upload_file_and_get_link(file_paths, input("パスワード>>"))))
+
+def main():
+    Ap = argparse.ArgumentParser()
+    Ap.add_argument("PATH", nargs='?', help="ファイルパスを入力")
+    Ap.add_argument("-nP", "--none-password", help="パスワードが不要な場合に使用")
+    Arguments = Ap.parse_args()
+    file_paths = ""
+    # 各引数について確認します。
+    if Arguments.none_password:
+        password = None
+        PATHs = Arguments.none_password
+    elif Arguments.PATH:
+        password = input("パスワード>> ")
+        PATHs = Arguments.PATH
+    if os.path.isdir(PATHs):
+        print(f"{PATHs} is a directory. Exiting.")
+    elif os.path.isfile(PATHs):
+        file_paths += PATHs + "\n"
+    else:
+        print(f"{PATHs} does not exist. Exiting.")
+    pyperclip.copy(upload_file_and_get_link(file_paths, password=password))
+
+if __name__ == '__main__':
+    main()
+
